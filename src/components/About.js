a447868a147280bd6a1dc7de442daa139cdc5fd4
Mwanza <victormwanza30@gmail.com>
2023-09-17 23:46:19 +0300

    additions

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,11 @@
 import React from "react";
-import './About.css'; 
-import about1 from '../images/about1.png'
-import about2 from '../images/about2.png'
-import about3 from '../images/about3.png'
-import about4 from '../images/about4.png'
-import about5 from '../images/about5.png'
-import about6 from '../images/about6.png'
+import "./About.css";
+import about1 from "../images/about1.png";
+import about2 from "../images/about2.png";
+import about3 from "../images/about3.png";
+import about4 from "../images/about4.png";
+import about5 from "../images/about5.png";
+import about6 from "../images/about6.png";
 
 export default function About() {
   return (
@@ -14,38 +14,85 @@ export default function About() {
 
       <div className="about-description">
         <p>
-          Ark Link Properties is a reputable real estate development company that stands at the forefront of creating innovative and sustainable residential and commercial properties. Our rich history and unwavering commitment to excellence have solidified our reputation as a trusted name in the industry.
+          Ark Link Properties is a reputable real estate development company
+          that stands at the forefront of creating innovative and sustainable
+          residential and commercial properties. Our rich history and
+          unwavering commitment to excellence have solidified our reputation as
+          a trusted name in the industry.
         </p>
 
         <div className="image-section">
-          <img src={about1} alt="Site Imag" className="about-image" />
-          <img src={about2} alt="Technical Imag" className="about-image" />
+          <div className="image-wrapper">
+            <img src={about1} alt="Site Imag" className="about-image" />
+            <div className="image-caption">Site Image 1</div>
+          </div>
+          <div className="image-wrapper">
+            <img src={about2} alt="Technical Imag" className="about-image" />
+            <div className="image-caption">Technical Image 2</div>
+          </div>
         </div>
 
         <p>
-          Our mission is to go beyond delivering high-quality construction, engineering, and architectural solutions. We aim to create sustainable and innovative spaces that not only exceed client expectations but also enhance communities and contribute to Kenya's holistic development.
+          Our mission is to go beyond delivering high-quality construction,
+          engineering, and architectural solutions. We aim to create sustainable
+          and innovative spaces that not only exceed client expectations but
+          also enhance communities and contribute to Kenya's holistic
+          development.
         </p>
         <div className="image-section">
-          <img src={about3} alt="Site Imag" className="about-image" />
-          <img src={about4} alt="Green Architecture Imag" className="about-image" />
+          <div className="image-wrapper">
+            <img src={about3} alt="Site Imag" className="about-image" />
+            <div className="image-caption">Site Image 3</div>
+          </div>
+          <div className="image-wrapper">
+            <img
+              src={about4}
+              alt="Green Architecture Imag"
+              className="about-image"
+            />
+            <div className="image-caption">Green Architecture Image 4</div>
+          </div>
         </div>
         <p>
-          We pride ourselves on meticulous attention to detail and an unwavering commitment to exceptional workmanship. At Ark Link, every project becomes a testament to our dedication to client satisfaction. Our team of professionals, from architects to engineers, ensures that creativity and expertise are at the heart of everything we do.
+          We pride ourselves on meticulous attention to detail and an
+          unwavering commitment to exceptional workmanship. At Ark Link, every
+          project becomes a testament to our dedication to client satisfaction.
+          Our team of professionals, from architects to engineers, ensures that
+          creativity and expertise are at the heart of everything we do.
         </p>
         <div className="image-section">
-          <img src={about5} alt="Site Imag" className="about-image" />
-          <img src={about6} alt="Green Architecture Imag" className="about-image" />
+          <div className="image-wrapper">
+            <img src={about5} alt="Site Imag" className="about-image" />
+            <div className="image-caption">Site Image 5</div>
+          </div>
+          <div className="image-wrapper">
+            <img
+              src={about6}
+              alt="Green Architecture Imag"
+              className="about-image"
+            />
+            <div className="image-caption">Green Architecture Image 6</div>
+          </div>
         </div>
         <p>
-          Embracing sustainability, we incorporate energy-efficient systems, groundbreaking technologies, and eco-friendly materials in all our projects. By staying updated with industry trends and advancements, we're always a step ahead, ensuring efficiency, cost-effectiveness, and environmental harmony.
+          Embracing sustainability, we incorporate energy-efficient systems,
+          groundbreaking technologies, and eco-friendly materials in all our
+          projects. By staying updated with industry trends and advancements,
+          we're always a step ahead, ensuring efficiency, cost-effectiveness,
+          and environmental harmony.
         </p>
 
         <p>
-          As we continue to make strides in the construction industry, our diverse portfolio showcases projects from luxury apartments to retail complexes. Each one bears the hallmark of our quality, innovation, and customer-centric approach.
+          As we continue to make strides in the construction industry, our
+          diverse portfolio showcases projects from luxury apartments to retail
+          complexes. Each one bears the hallmark of our quality, innovation,
+          and customer-centric approach.
         </p>
 
         <p className="conclusion">
-          Join us in our journey. Together, let's shape the future of real estate and construction in Kenya and aim for a brighter, sustainable tomorrow.
+          Join us in our journey. Together, let's shape the future of real
+          estate and construction in Kenya and aim for a brighter, sustainable
+          tomorrow.
         </p>
       </div>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,101 +1,189 @@
-import React from "react";
-import './Home.css';
-import Bungalows from '../images/Bungalow.png'
-import CountryClub from '../images/CountryClub.png'
-import Maisonette from '../images/Maisonete.png'
-import PetrolStation from '../images/PetrolStation.png'
-import Restaurant from '../images/Restaurant.png'
-import YunaCityMall from '../images/YunaCityMall.png'
-import yunafarmresort from '../images/yunafarmresort.png'
-import YunaHomes from '../images/YunaHomes.png'
+import React, { useState, useEffect } from "react";
+import "./Home.css";
+import YunaHomes from "../images/YunaHomes.png";
+import CountryClub from "../images/CountryClub.png";
+import Maisonette from "../images/Maisonete.png";
+import PetrolStation from "../images/PetrolStation.png";
+import Restaurant from "../images/Restaurant.png";
 
 export default function Home() {
-    return (
-        <div className="home-container">
-            <h1>Welcome to Ark Link Properties</h1>
-            <p className="intro-text">Introducing the revolutionary Yuna City Development project. Immerse yourself in luxury, innovation, and sustainability.</p>
+  const carouselImages = [
+    { url: YunaHomes, alt: "Yuna Homes" },
+    { url: CountryClub, alt: "Country Club" },
+    { url: Maisonette, alt: "Maisonette" },
+    { url: PetrolStation, alt: "Petrol Station" },
+    { url: Restaurant, alt: "Restaurant" },
+  ];
 
-            <div className="yuna-section">
-                <img src={yunafarmresort} alt="Yuna Farm Resort" className="yuna-image" />
-                <div className="yuna-details">
-                <h2 style={{color: 'gold'}}>Yuna Farm Resort</h2>
-                    <p>The farm resort is a unique blend of modern living and traditional farming practices, offering visitors the best of both worlds. Experience immersive farm life in a vast land of opportunities.</p>
-                </div>
-            </div>
-            <div className="yuna-section">
-                <img src={YunaHomes} alt="Yuna Farm Resort" className="yuna-image" />
-                <div className="yuna-details">
-                    <h2 style={{color: 'gold'}}>Yuna Homes</h2>
-                    <p>With a focus on providing comfortable
-                        and sustainable living spaces, the residential area of
-                        the development has been carefully planned and
-                        designed to meet the diverse needs of the residents.</p>
-                </div>
-            </div>
-            <div className="yuna-section">
-                <img src={Maisonette} alt="Yuna Farm Resort" className="yuna-image" />
-                <div className="yuna-details">
-                <h2 style={{color: 'gold'}}>Maisonette</h2>
-                    <p>These luxurious units offer spacious living areas with all rooms ensuite, 
-                      allowing for maximum comfort and privacy. The double-volume lounge area adds
-                      a touch of elegance and sophistication to the design, making it a
-                      perfect space for entertaining guests or spending quality time with family.</p>
-                </div>
-            </div>
-            <div className="yuna-section">
-                <img src={Bungalows} alt="Yuna Farm Resort" className="yuna-image" />
-                <div className="yuna-details">
-                <h2 style={{color: 'gold'}}>Bungalows</h2>
-                    <p>Yuna Homes also features 9 spacious and luxurious bungalows, each designed
-                      with modern aesthetics and functionality in mind. These flat-roof bungalows come
-                      with a total area of 330 square meters and are equipped with all the necessary
-                      amenities for a comfortable and luxurious lifestyle.</p>
-                </div>
-            </div>
-            <div className="yuna-section">
-                <img src={YunaCityMall} alt="Yuna Farm Resort" className="yuna-image" />
-                <div className="yuna-details">
-                <h2 style={{color: 'gold'}}>Yuna City Mall</h2>
-                    <p>Yuna City Development's Commercial Center is a one-stop shop for all your
-                        commercial needs.</p>
-                </div>
-            </div>
-            <div className="yuna-section">
-                <img src={PetrolStation} alt="Yuna Farm Resort" className="yuna-image" />
-                <div className="yuna-details">
-                <h2 style={{color: 'gold'}}>Petrol Station and Convenience Store</h2>
-                    <p>A drive-thru petrol station is located within the premises, allowing for easy 
-                      access to fuel up your car while running errands or on the way to work.</p>
-                </div>
-            </div>
-            <div className="yuna-section">
-                <img src={Restaurant} alt="Yuna Farm Resort" className="yuna-image" />
-                <div className="yuna-details">
-                    <h2 style={{color: 'gold'}}>Restaurant and Club</h2>
-                    <p>The restaurant is a modern masterpiece, with a sleek bamboo exterior that exudes
-                      elegance and sophistication..</p>
-                </div>
-            </div>
-            <div className="yuna-section">
-                <img src={CountryClub} alt="Yuna Farm Resort" className="yuna-image" />
-                <div className="yuna-details">
-                    <h2 style={{color: 'gold'}}>Country Club and Cottages</h2>
-                    <p>The Country Club and Cottages at Yuna City Development are the perfect escape from the
-                      hustle and bustle of the city. Surrounded by the beauty of nature, these facilities provide a serene
-                      environment where visitors can relax and unwind.</p>
-                </div>
-            </div>
+  const [currentImage, setCurrentImage] = useState(0);
 
-            <div>
-            <p className="italic-justified">
-                An already published magazine is available..
-            </p>
-             </div>
-                <div className="cta">
-                <h2>Experience Yuna City</h2>
-                <p>Discover unparalleled luxury, innovation, and sustainability with Ark Link Properties. 
-                  Join us in shaping the future of real estate and construction in Kenya.</p>
-            </div>
-        </div>
+  // Function to go to the next image
+  const nextImage = () => {
+    setCurrentImage((prevImage) =>
+      prevImage === carouselImages.length - 1 ? 0 : prevImage + 1
     );
+  };
+
+  // Function to go to the previous image
+  const prevImage = () => {
+    setCurrentImage((prevImage) =>
+      prevImage === 0 ? carouselImages.length - 1 : prevImage - 1
+    );
+  };
+
+  useEffect(() => {
+    // Set an interval to change the image every 5 seconds (5000 milliseconds)
+    const interval = setInterval(nextImage, 5000);
+
+    // Clear the interval when the component unmounts
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div className="home-container">
+      <h1>Welcome to Ark Link Properties</h1>
+      <p className="intro-text">SUSTAINABILITY INNOVATION INTEGRITY</p>
+
+      <div className="carousel">
+        <img
+          src={carouselImages[currentImage].url}
+          alt={carouselImages[currentImage].alt}
+          className="carousel-image"
+        />
+
+        <div className="carousel-navigation">
+          <button onClick={prevImage} className="carousel-button">
+            {"< Prev"}
+          </button>
+          <button onClick={nextImage} className="carousel-button">
+            {"Next >"}
+          </button>
+        </div>
+      </div>
+
+      <div>
+        <p className="italic-justified">An already published magazine is available..</p>
+      </div>
+      <div className="cta">
+        <h2>Experience Yuna City</h2>
+        <p>
+          Ark Link Properties is a dynamic and innovative private company founded by Ian Ruhiu, a graduate architect, in 2021. We are proud to be a youth-led organization that believes in empowering young professionals to excel in the construction and engineering industry. Our company, incorporated on February 7th, 2023, under the laws of Kenya, specializes in providing a wide range of services in civil works, building works, construction services, and engineering and architectural works and supplies.
+        </p>
+      </div>
+    </div>
+  );
 }
+
+// import React from "react";
+// import { Carousel } from "react-responsive-carousel";
+
+// import './Home.css';
+// import Bungalows from '../images/Bungalow.png'
+// import CountryClub from '../images/CountryClub.png'
+// import Maisonette from '../images/Maisonete.png'
+// import PetrolStation from '../images/PetrolStation.png'
+// import Restaurant from '../images/Restaurant.png'
+// import YunaCityMall from '../images/YunaCityMall.png'
+// import yunafarmresort from '../images/yunafarmresort.png'
+// import YunaHomes from '../images/YunaHomes.png'
+
+
+// const carouselImages = [
+//     { url: YunaCityMall, alt: "other 1" },
+//     { url: Restaurant, alt: "other 2" },
+//     { url: Maisonette, alt: "other 3" },
+//     { url: YunaHomes, alt: "other 4" },
+//     { url: Bungalows, alt: "other 5" },
+//   ];
+// export default function Home() {
+//     return (
+//         <div className="home-container">
+//             <h1>Welcome to Ark Link Properties</h1>
+//             <p className="intro-text">Introducing the revolutionary Yuna City Development project. Immerse yourself in luxury, innovation, and sustainability.</p>
+
+//             <div className="yuna-section">
+//                 <img src={yunafarmresort} alt="Yuna Farm Resort" className="yuna-image" />
+//                 <div className="yuna-details">
+//                 <h2 style={{color: 'gold'}}>Yuna Farm Resort</h2>
+//                     <p>The farm resort is a unique blend of modern living and traditional farming practices, offering visitors the best of both worlds. Experience immersive farm life in a vast land of opportunities.</p>
+//                 </div>
+//             </div>
+//             <div className="yuna-section">
+//                 <img src={YunaHomes} alt="Yuna Farm Resort" className="yuna-image" />
+//                 <div className="yuna-details">
+//                     <h2 style={{color: 'gold'}}>Yuna Homes</h2>
+//                     <p>With a focus on providing comfortable
+//                         and sustainable living spaces, the residential area of
+//                         the development has been carefully planned and
+//                         designed to meet the diverse needs of the residents.</p>
+//                 </div>
+//             </div>
+//             <div className="yuna-section">
+//                 <img src={Maisonette} alt="Yuna Farm Resort" className="yuna-image" />
+//                 <div className="yuna-details">
+//                 <h2 style={{color: 'gold'}}>Maisonette</h2>
+//                     <p>These luxurious units offer spacious living areas with all rooms ensuite, 
+//                       allowing for maximum comfort and privacy. The double-volume lounge area adds
+//                       a touch of elegance and sophistication to the design, making it a
+//                       perfect space for entertaining guests or spending quality time with family.</p>
+//                 </div>
+//             </div>
+//             <div className="yuna-section">
+//                 <img src={Bungalows} alt="Yuna Farm Resort" className="yuna-image" />
+//                 <div className="yuna-details">
+//                 <h2 style={{color: 'gold'}}>Bungalows</h2>
+//                     <p>Yuna Homes also features 9 spacious and luxurious bungalows, each designed
+//                       with modern aesthetics and functionality in mind. These flat-roof bungalows come
+//                       with a total area of 330 square meters and are equipped with all the necessary
+//                       amenities for a comfortable and luxurious lifestyle.</p>
+//                 </div>
+//             </div>
+//             <div className="yuna-section">
+//                 <img src={YunaCityMall} alt="Yuna Farm Resort" className="yuna-image" />
+//                 <div className="yuna-details">
+//                 <h2 style={{color: 'gold'}}>Yuna City Mall</h2>
+//                     <p>Yuna City Development's Commercial Center is a one-stop shop for all your
+//                         commercial needs.</p>
+//                 </div>
+//             </div>
+//             <div className="yuna-section">
+//                 <img src={PetrolStation} alt="Yuna Farm Resort" className="yuna-image" />
+//                 <div className="yuna-details">
+//                 <h2 style={{color: 'gold'}}>Petrol Station and Convenience Store</h2>
+//                     <p>A drive-thru petrol station is located within the premises, allowing for easy 
+//                       access to fuel up your car while running errands or on the way to work.</p>
+//                 </div>
+//             </div>
+//             <div className="yuna-section">
+//                 <img src={Restaurant} alt="Yuna Farm Resort" className="yuna-image" />
+//                 <div className="yuna-details">
+//                     <h2 style={{color: 'gold'}}>Restaurant and Club</h2>
+//                     <p>The restaurant is a modern masterpiece, with a sleek bamboo exterior that exudes
+//                       elegance and sophistication..</p>
+//                 </div>
+//             </div>
+//             <div className="yuna-section">
+//                 <img src={CountryClub} alt="Yuna Farm Resort" className="yuna-image" />
+//                 <div className="yuna-details">
+//                     <h2 style={{color: 'gold'}}>Country Club and Cottages</h2>
+//                     <p>The Country Club and Cottages at Yuna City Development are the perfect escape from the
+//                       hustle and bustle of the city. Surrounded by the beauty of nature, these facilities provide a serene
+//                       environment where visitors can relax and unwind.</p>
+//                 </div>
+//             </div>
+
+//             <div>
+//             <p className="italic-justified">
+//                 An already published magazine is available..
+//             </p>
+//              </div>
+//                 <div className="cta">
+//                 <h2>Experience Yuna City</h2>
+//                 <p>Discover unparalleled luxury, innovation, and sustainability with Ark Link Properties. 
+//                   Join us in shaping the future of real estate and construction in Kenya.</p>
+//             </div>
+//         </div>
+//     );
+// }
